a28145bc3ae6035719a1a07c462ccaccc610de7a
HFsa-RaShid <hafsa20cse028@gmail.com>
2026-03-04 09:59:28 +0600

    update

--- server.js
+++ server.js
@@ -18,10 +18,31 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cookieParser());
 
-app.use(cors({
-  origin: "https://beautyzone-client.vercel.app", 
-  credentials: true,               
-}));
+// app.use(cors({
+//   origin:"https://beautyzone-client.vercel.app" ,
+//   credentials: true,               
+// }));
+
+const allowedOrigins = [
+  "http://127.0.0.1:5500",
+  "https://beautyzone-client.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 
 
 
